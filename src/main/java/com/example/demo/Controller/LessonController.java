4f193bda031592327d4598e044f241c94646c1b3
factoryuser1 <factoryuser1@yahoo.com>
2021-09-23 14:15:10 -0700

    Initial commit

---
+++ src/main/java/com/example/demo/Controller/LessonController.java
@@ -0,0 +1,48 @@
+package com.example.demo.Controller;
+
+
+import com.example.demo.Model.Lesson;
+import com.example.demo.Repository.LessonRepository;
+import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.Column;
+import java.util.Collections;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/lessons")
+public class LessonController {
+    //inject the lesson repository using constructor injection
+    private final LessonRepository lessonTable;
+
+    public LessonController(LessonRepository lessonTable){
+        this.lessonTable = lessonTable;
+    }
+
+    @GetMapping("")
+    public Iterable<Lesson> finAll() {
+        return this.lessonTable.findAll();
+    }
+
+    @PostMapping("")
+    public Lesson createLesson(@RequestBody Lesson lesson){
+        return this.lessonTable.save(lesson);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Lesson> getLessonById(@PathVariable Long id){
+        Optional<Lesson> lesson = lessonTable.findById(id);
+
+        return lesson;
+    }
+    @DeleteMapping("/{id}")
+    public String deleteLessonById(@PathVariable Long id){
+        try{
+            lessonTable.deleteById(id);
+            return "Lesson " + id + " was deleted successfully!";
+        } catch (Exception e){
+            return "Unable to delete lesson #: " + id;
+        }
+
+    }
+}
---
+++ test.sql
@@ -0,0 +1,6 @@
+insert into lessons (title, delivered_on) values ('Requests and Responses', '2017-02-05');
+insert into lessons (title, delivered_on) values ('SQL', '2017-04-06');
+insert into lessons (title, delivered_on) values ('Bellman Ford', '2017-05-02');
+insert into lessons (title, delivered_on) values ('Spring 5', '2018-02-05');
+insert into lessons (title, delivered_on) values ('Spring Boot', '2017-04-06');
+insert into lessons (title, delivered_on) values ('Ammar', '2020-05-02');
